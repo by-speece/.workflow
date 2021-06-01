d6a704360e4941ecd83e13bd3749021ebd79c93b
By Speece <72341542+by-speece@users.noreply.github.com>
2021-06-01 19:17:56 +0200

    added breeze

--- Data/Bash/rice.sh
+++ Data/Bash/rice.sh
@@ -1,6 +1,6 @@
 sudo pacman -Syu i3-gaps wget curl gedit repose zsh screenkey htop gtop dunst gnome-screenshot udiskie feh alacritty neofetch xdotool light signal-desktop ranger rofi nautilus pavucontrol --needed
 sudo pacman -S libreoffice-fresh libreoffice-fresh-pl firefox thunderbird mpv youtube-dl filezilla discord calibre audacity  obs-studio scribus keepassxc krita rawtherapee inkscape gimp --needed
-sudo pacman -S papirus-icon-theme adapta-gtk-theme lxappearance-gtk3 gtk-chtheme kvantum-qt5 --needed
+sudo pacman -S papirus-icon-theme adapta-gtk-theme lxappearance-gtk3 gtk-chtheme breeze breeze-gtk kvantum-qt5 --needed
 sudo pacman -S blueman bluez bluez-utils cups cups-pdf avahi gutenprint foomatic-db-gutenprint-ppds syncthing --needed
 sudo pacman -S ttf-bitstream-vera ttf-croscore ttf-dejavu gnu-free-fonts adobe-source-han-sans-jp-fonts  adobe-source-han-serif-jp-fonts otf-ipafont ttf-hanazono --needed
 sudo systemctl enable cups 
